0ddb83c20d35657d5ff0b1c4ee73c0609f0ac7d9
Dima <dima0morgunov@gmail.com>
2018-08-04 23:12:45 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+console.clear();
+const getTitle = document.getElementById("title")
+const getMessage = document.getElementById("message")
+class task{
+    conteiner(){
+        this.title = "text"
+        this.message = "message"
+    }
+
+}
+document.getElementById("createList").addEventListener("click", () => {
+    console.log('Clicked')})
+
+console.log(getTitle, getMessage)
