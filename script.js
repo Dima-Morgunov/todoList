c97e7c979396e8d2c06cb441aa2bacf8dc71f243
Dima-Morgunov <dima0morgunov@gmail.com>
2018-08-06 01:20:39 +0300

    aster

--- script.js
+++ script.js
@@ -1,14 +1,28 @@
 console.clear();
 const getTitle = document.getElementById("title")
 const getMessage = document.getElementById("message")
-class task{
-    conteiner(){
+class Task{
+    constructor(){
         this.title = "text"
         this.message = "message"
     }
 
 }
-document.getElementById("createList").addEventListener("click", () => {
-    console.log('Clicked')})
 
-console.log(getTitle, getMessage)
+
+document.getElementById("createList").addEventListener("click", () => {
+    const createNewTask = document.createElement("div");
+    const createTaskTittle = document.createElement("h3");
+    const createTaskText = document.createElement("span");
+    const elementId = createNewTask.id = randomId(1,9999999);
+    todolist.appendChild(createNewTask);
+
+
+    elementId.appendChild(createTaskTittle);
+    console.log(elementId)
+})
+
+
+const randomId = (min, max) =>{
+        return Math.floor(Math.random() * (max - min)) + min;
+}
