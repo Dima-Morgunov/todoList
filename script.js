21e3cb223272555d97dfd3dd80de961f95d09fe2
Dima-Morgunov <dima0morgunov@gmail.com>
2018-08-09 00:54:28 +0300

    aster

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 console.clear();
 const getTitle = document.getElementById("title")
 const getMessage = document.getElementById("message")
-const button = document.querySelector('#createList')
-const todolist = document.querySelector('#todolist')
+const button = document.getElementById('createList')
+const todolist = document.getElementById('todolist')
 class Task{
-    constructor(title, message){
+    constructor(title, message, randomId){
         this.title = title
         this.message = message
+        this.todoId = randomId
     }
-
 }
 
-const TasksArr = []
+let TasksArr = []
 
 
 const randomId = (min, max) =>{
@@ -34,24 +34,49 @@ const randomId = (min, max) =>{
 })*/
 
 const createTask = (title, message) => {
-    const task = new Task(title, message)
+    const task = new Task(title, message, randomId(1, 999999))
     TasksArr.push(task)
 
     todolist.innerHTML = ' '
 
     drawTasks()
 
-
     console.log(TasksArr)
+}
+
+function deleteTask(id) {
+
+
+    //noinspection JSAnnotator
+    TasksArr = TasksArr.filter(function(e) {
+        return e.todoId != id
+    })
+
+
+
+    todolist.innerHTML = ' '
+
+    drawTasks()
+    console.log(TasksArr)
+
+
+    console.log(id)
 }
 
 function drawTasks() {
     TasksArr.forEach(e => {
         let span = document.createElement('div')
+        let drawTodoTitle = document.createElement("h3")
+        let deleteTodo = document.createElement("button")
+
         span.innerText = e.message
+        drawTodoTitle.innerText = e.title
+        deleteTodo.addEventListener('click', ()=> deleteTask(e.todoId))
+
+        todolist.appendChild(drawTodoTitle)
         todolist.appendChild(span)
+        todolist.appendChild(deleteTodo)
     })
-v
 }
 
 
